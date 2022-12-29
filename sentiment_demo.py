096b1850d135aa8eea0f558e8de96b2f2c9ae033
Paul <paulflorentin.pf@gmail.com>
2022-12-29 08:30:50 +0200

    Added git submodule for the tested API

--- sentiment_demo.py
+++ sentiment_demo.py
@@ -1,12 +1,4 @@
 import requests
-import json
-
-
-def simple_response(text):
-    to_strip = '{}"sentiment":\n '
-    new_text = text.translate({ord(i): None} for i in to_strip.split())
-    return new_text
-
 
 text = "I love python"
 
@@ -15,4 +7,9 @@ url = "http://127.0.0.1:5000?q={}".format(text)
 response = requests.request("GET", url, data=text)
 
 print(response.text)
-print(simple_response(response.text))
+
+"""
+To be implemented:
+A method that extracts only the feedback from the sentiment analysis. 
+Ex: Positive/Negative, without JSON format.
+"""
